f4556825bb8b2c00a83c57ae405b6bbae37ab75d
Isher Sagay <ishersagay@gmail.com>
2021-09-19 19:39:13 +0100

    Everything works and is very much complete, however I've ran into a bit of an issue with the commas buts its good

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import {arrToMorse} from "./translator.js";
 
 const translate = document.querySelector(".morse__btn");
+const output = document.querySelector(".morse_output");
 
 
 translate.addEventListener("click", evt => {
  const userInput = document.querySelector(".english_value").value
  let morseCode = arrToMorse(userInput)
+ output.innerHTML = morseCode
  console.log(morseCode)
 })
